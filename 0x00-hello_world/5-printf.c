2251d3bcf48cfcedd6b59e5943b611b919d0aca4
kiplrry <kiplarry19@gmail.com>
2022-09-08 15:53:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * Printf.c
+ * Auth: Larry
+ */
+include < stdio.h >
+/**
+ * main - Prints the string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
