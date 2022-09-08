f67043cc7b049ee5f6270d14c837c29fcb0e7ffd
kiplrry <kiplarry19@gmail.com>
2022-09-08 15:31:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * puts.c 
+ * Auth : Larry
+ */
+#include <stdio.h>
+/*
+ * Main - is used as the main progeram function
+ * 
+ * return : 0
+ */
+int main(void)
+{
+	char str1[51] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
