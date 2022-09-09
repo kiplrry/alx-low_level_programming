1fdcb29e3c8e2025dba73a2fc26e77cf85c83ef8
kiplrry <kiplarry19@gmail.com>
2022-09-09 13:13:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - runs main program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
