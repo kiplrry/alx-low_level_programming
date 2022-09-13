de1ead417888d651767a5e85e82ed9e02c8f7cc4
kiplrry <kiplarry19@gmail.com>
2022-09-13 07:08:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include  "main.h"
+/**
+ * main - runs main program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - runs main program
  *
  * Return: 0
  */
+int _putchar(char c);
+int letter(void);
+void print_alphabet(void);
+
 int letter(void)
 {
 	printf("_putchar\n");
 
 	return (0);
+}
+void print_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return;
+}
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
 }
