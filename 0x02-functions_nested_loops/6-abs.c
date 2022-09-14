c78f20e6c6545e64f37d0cad8f7248e07b8bd8f1
kiplrry <kiplarry19@gmail.com>
2022-09-14 11:51:54 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolut of an integer
+ *@n: the integer to be computed
+ *
+ * Return: the absolute
+ */
+int _abs(int n)
+{
+	int ab;
+
+	if (n < 0)
+		ab = n * -1;
+
+	return (ab);
+}
