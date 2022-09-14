7d39ad930736c52a1fe8626c4e643712b99a67b6
kiplrry <kiplarry19@gmail.com>
2022-09-14 02:01:31 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char c;
+
+	while (num <= 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		num++;
+	}
+}
