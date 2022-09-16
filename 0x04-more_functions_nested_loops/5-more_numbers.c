df688f171f63d520201aef18a47729cfea6d352d
kiplrry <kiplarry19@gmail.com>
2022-09-16 14:06:51 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int r;
+	int d;
+
+	for (r = 0; r <= 10; r++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			_putchar(d - '0');
+		}
+		_putchar('\n');
+	}
+}
