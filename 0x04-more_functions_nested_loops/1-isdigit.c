3b482c5fb2fca5e19649087ceee327c10ea5d4a5
kiplrry <kiplarry19@gmail.com>
2022-09-16 12:55:02 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - checks if it si a digit between 0 and 10
+ *
+ * @c: integer to be checked
+ *
+ * Return: 0 fail 1 success
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 10))
+		return (1);
+	else
+		return (0);
+}
