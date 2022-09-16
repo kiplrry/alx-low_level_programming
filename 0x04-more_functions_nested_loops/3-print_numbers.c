a3f36da7681a71325d05059c1deebba8a733efd0
kiplrry <kiplarry19@gmail.com>
2022-09-16 13:37:12 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int r;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
