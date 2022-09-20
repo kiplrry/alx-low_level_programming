bbb4960affaa5ece4b951f819aed97d057f6f8ba
kiplrry <kiplarry19@gmail.com>
2022-09-20 11:46:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints reverse
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	len--;
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
