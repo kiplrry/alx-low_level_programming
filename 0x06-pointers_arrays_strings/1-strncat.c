1793e439e62fa1772a5355dc6778fe002fb21d16
kiplrry <kiplarry19@gmail.com>
2022-09-22 13:18:27 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatnates src to dest up to n
+ * @src: source
+ * @dest: destination
+ * @n: the number of bytes to be copied
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen, slen, len, i;
+
+	slen = dlen = len = 0;
+
+	while (dest[dlen])
+		dlen++;
+	while (src[slen])
+		slen++;
+	if (n < slen)
+		n = slen;
+	for (i = dlen; i < n + dlen; i++)
+	{
+		dest[i] = src[len];
+		len++;
+	}
+	dest[n + dlen] = '\0';
+	return (dest);
+}
