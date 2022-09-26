aa676367c1b7a12af2a6df702edfa5daccbab9df
kiplrry <kiplarry19@gmail.com>
2022-09-26 14:18:58 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - copies a byte of data to n number of location from  s
+ * @s: initial memory location
+ * @b: byte to be copied
+ * @n: size
+ * Return: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
