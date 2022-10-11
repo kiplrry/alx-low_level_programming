77350e5214e4eed17f7e1bd834666a8f228f4fea
kiplrry <kiplarry19@gmail.com>
2022-10-11 14:00:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - defines a struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+sruct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
