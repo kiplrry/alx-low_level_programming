68c91cc3970ef45ed3e7886778d5c9f05ce832e7
kiplrry <kiplarry19@gmail.com>
2022-10-28 10:09:50 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get node at n
+ * @head: head pointer
+ * @index: index to retun
+ * Return: NULL or pointer to the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int r = 0;
+	listint_t *tmp;
+
+	tmp = head;
+	while (head)
+	{
+		head = head->next;
+		r++;
+		if (r == index)
+			return (tmp);
+	}
+	return (NULL);
+}
