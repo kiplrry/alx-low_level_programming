8638daf87d3ad2950548d7342d34e82fd1aff9d2
kiplrry <kiplarry19@gmail.com>
2023-01-09 17:39:30 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -25,12 +25,3 @@ for (i = 0; i < newTable->size; i++)
     newTable->array[i] = NULL;
 return (newTable);
 }
-
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
-}
